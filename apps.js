07e2f8fb3925e3a8111fe495bbd992367b1a2f77
satyavk <n160241@rguktn.ac.in>
2021-10-16 19:10:11 +0530

    mistakes found

--- apps.js
+++ apps.js
@@ -24,6 +24,6 @@ app.post("/",function(req,res){
   console.log(Items);
   res.redirect("/");
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("server is started");
 });
